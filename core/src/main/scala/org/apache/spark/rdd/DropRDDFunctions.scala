83cf877c22944913ed589fbf8efb3a7e75f51c17
Erik Erlandson <eerlands@redhat.com>
2014-07-26 14:11:44 -0700

    refactored to use PromiseRDD

--- core/src/main/scala/org/apache/spark/rdd/DropRDDFunctions.scala
+++ core/src/main/scala/org/apache/spark/rdd/DropRDDFunctions.scala
@@ -19,7 +19,7 @@ package org.apache.spark.rdd
 
 import scala.reflect.ClassTag
 
-import org.apache.spark.{Logging, Partition, TaskContext}
+import org.apache.spark.{SparkContext, Logging, Partition, TaskContext}
 import org.apache.spark.{Dependency, NarrowDependency, OneToOneDependency}
 
 // Assuming child RDD type having only one partition
@@ -36,14 +36,28 @@ class FanInDep[T: ClassTag](rdd: RDD[T]) extends NarrowDependency[T](rdd) {
   override def getParents(pid: Int) = List(0)
 }
 
-class UnitaryPartition extends Partition {
+// A PromiseRDD has exactly one partition, by construction:
+class PromisePartition extends Partition {
   override def index = 0
 }
 
-class DropPartition[V: ClassTag](ppart: Partition, locRDD: RDD[V]) extends Partition {
+// A way to represent the concept of a promised expression as an RDD, so that it
+// can operate inside the lazy-transform formalism
+class PromiseRDD[V: ClassTag](expr: TaskContext => V, context: SparkContext, deps: Seq[Dependency[_]])
+  extends RDD[V](context, deps) {
+
+  // This RDD has exactly one partition by definition, since it will contain
+  // a single row holding the 'promised' result of evaluating 'expr' 
+  override def getPartitions = Array(new PromisePartition)
+
+  // compute evaluates 'expr', yielding an iterator over a sequence of length 1:
+  override def compute(p: Partition, ctx: TaskContext) = List(expr(ctx)).iterator
+}
+
+class DropPartition[V: ClassTag](ppart: Partition, locRDD: PromiseRDD[V]) extends Partition {
   override def index = ppart.index
   def pp = ppart
-  def loc(ctx: TaskContext): V = locRDD.iterator(new UnitaryPartition, ctx).next
+  def loc(ctx: TaskContext): V = locRDD.iterator(new PromisePartition, ctx).next
 }
 
 /**
@@ -52,46 +66,17 @@ class DropPartition[V: ClassTag](ppart: Partition, locRDD: RDD[V]) extends Parti
  */
 class DropRDDFunctions[T : ClassTag](self: RDD[T]) extends Logging with Serializable {
 
-  // A way to represent the concept of applying a function to an RDD to get a single value,
-  // inside the lazy transform formalism
-  // Returns a specialized RDD that contains a single row which is the value returned
-  // from 'f' applied to the partitions of the parent RDD
-  // This version works on Iterator[T] and is something you could present to user visible API
-  // Note this version forces computation of all parent partitions
-  def applyFunctionToPartitions[V: ClassTag](f: Seq[Iterator[T]] => V): RDD[V] = {
-    new RDD[V](self.context, List(new FanOutDep(self))) {
-      // If there is a way to get list of parent partitions from worker environment, 
-      // I could just keep parent without pre-caching the partition array
-      val parent:RDD[T] = self
-      val plist = parent.partitions
-
-      // this RDD has just one partition, since it only holds a single value in a single row
-      override def getPartitions: Array[Partition] = Array(new UnitaryPartition)
-
-      // to compute the single output value, apply the function to the sequence of parent partitions
-      override def compute(prt: Partition, ctx: TaskContext): Iterator[V] = {
-        Array(f(plist.map(s => parent.iterator(s, ctx)))).iterator
-      }
-    }
+  /**
+   * Return a PromiseRDD by applying function 'f' to the partitions of this RDD
+   */
+  def promiseFromPartitions[V: ClassTag](f: Seq[Iterator[T]] => V): PromiseRDD[V] = {
+    val expr = (ctx: TaskContext) => f(self.partitions.map(s => self.iterator(s, ctx)))
+    new PromiseRDD[V](expr, self.context, List(new FanOutDep(self))) 
   }
 
-  // This variation is suited for internal use
-  // works directly on sequence of Partition structures
-  // This version can be specific about which parent partitions it invokes compute on
-  def applyFunctionToPartitionArray[V: ClassTag](f: (Array[Partition], RDD[T], TaskContext) => V): RDD[V] = {
-    new RDD[V](self.context, List(new FanOutDep(self))) {
-      // is it possible to get partition list from worker environment without pre-caching it?
-      val parent:RDD[T] = self
-      val plist:Array[Partition] = parent.partitions
-
-      // always a single partition, which will contain a single row
-      override def getPartitions: Array[Partition] = Array(new UnitaryPartition)
-
-      // generate a single row, containing result of applying 'f' to sequence of parent's partitions
-      override def compute(prt: Partition, ctx: TaskContext): Iterator[V] = {
-        Array(f(plist, parent, ctx)).iterator
-      }
-    }
+  def promiseFromPartitionArray[V: ClassTag](f: (Array[Partition], RDD[T], TaskContext) => V): PromiseRDD[V] = {
+    val expr = (ctx: TaskContext) => f(self.partitions, self, ctx)
+    new PromiseRDD[V](expr, self.context, List(new FanOutDep(self))) 
   }
 
 
@@ -121,7 +106,7 @@ class DropRDDFunctions[T : ClassTag](self: RDD[T]) extends Logging with Serializ
       }
     }
 
-    val locRDD = this.applyFunctionToPartitionArray(locate)
+    val locRDD = this.promiseFromPartitionArray(locate)
 
     new RDD[T](self.context, List(new OneToOneDependency(self), new FanInDep(locRDD))) {
       override def getPartitions: Array[Partition] = self.partitions.map(p => new DropPartition(p, locRDD))
@@ -137,6 +122,44 @@ class DropRDDFunctions[T : ClassTag](self: RDD[T]) extends Logging with Serializ
     }
   }
 
+  def drop2(n: Int):RDD[T] = {
+    if (n <= 0) return self
+
+    // locate partition that includes the nth element
+    val locate = (iter: Seq[Iterator[T]]) => {
+      var rem = n
+      var p = 0
+      var np = 0
+      while (rem > 0  &&  p < iter.length) {
+        np = iter(p).length
+        rem -= np
+        p += 1
+      }
+
+      if (rem > 0  ||  (rem == 0  &&  p >= iter.length)) {
+        // all elements were dropped
+        (p, 0)
+      } else {
+        // (if we get here, note that rem <= 0)
+        (p - 1, np + rem)
+      }
+    }
+
+    val locRDD = this.promiseFromPartitions(locate)
+
+    new RDD[T](self.context, List(new OneToOneDependency(self), new FanInDep(locRDD))) {
+      override def getPartitions: Array[Partition] = self.partitions.map(p => new DropPartition(p, locRDD))
+      override val partitioner = self.partitioner
+      override def compute(split: Partition, ctx: TaskContext):Iterator[T] = {
+        val dp = split.asInstanceOf[DropPartition[(Int, Int)]]
+        val (pFirst, pDrop) = dp.loc(ctx)
+        val parent = firstParent[T]
+        if (dp.index > pFirst) return parent.iterator(dp.pp, ctx)
+        if (dp.index == pFirst) return parent.iterator(dp.pp, ctx).drop(pDrop)
+        Iterator.empty
+      }
+    }
+  }
 
 /**
  * Return a new RDD formed by dropping the last (n) elements of the input RDD
